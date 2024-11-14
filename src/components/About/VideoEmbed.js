449695e386a45bc5cedd41daed7b05df9edf6fec
Utkarsha <47937976+Utkarsha666@users.noreply.github.com>
2024-11-14 13:02:46 +0000

    *Promo video

--- src/components/About/VideoEmbed.js
+++ src/components/About/VideoEmbed.js
@@ -18,12 +18,12 @@ const VideoEmbed = () => {
       <iframe
         width="100%"
         height="315"
-        src="https://www.youtube.com/embed/W04aJAqnDHE?si=d4zdmGSHMTGjzLeB"
-        title="About"
-        frameBorder="0"
+        src="https://www.youtube.com/embed/vkr2XPKrjGs?si=OtylUeCJqeoSZb-S"
+        title="YouTube video player"
+        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
       ></iframe>
     </Box>
   );
