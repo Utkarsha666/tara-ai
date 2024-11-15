e8bc862888c08086b95e9bc8a667060590d09a24
Utkarsha <47937976+Utkarsha666@users.noreply.github.com>
2024-11-15 20:02:59 +0000

    *video update

--- src/components/About/VideoEmbed.js
+++ src/components/About/VideoEmbed.js
@@ -18,7 +18,7 @@ const VideoEmbed = () => {
       <iframe
         width="100%"
         height="315"
-        src="https://www.youtube.com/embed/JNzsKIAVPns?si=7z_qn7bIlwEGVkqp"
+        src="https://www.youtube.com/embed/OE-m9bSoPtI?si=8sexEEbnSIbR1cRK"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
